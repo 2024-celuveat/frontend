5cf0cb47aa890a18384e5976f40fbb38e7790854
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-07-20 00:02:00 +0900

    refactor: 폰트 최적화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -6,13 +7,18 @@ export const metadata: Metadata = {
   description: '셀럽 추천 맛집 소개 서비스, 셀럽잇',
 };
 
+const myFont = localFont({
+  src: './PretendardVariable.woff2',
+  display: 'block',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ko'>
+    <html lang='ko' className={myFont.className}>
       <body className='relative max-w-[495px] min-h-svh my-0 mx-auto shadow-lg'>
         {children}
       </body>
