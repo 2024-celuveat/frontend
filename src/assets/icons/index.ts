497dad8a3e4f955b10d7ac27d898eb49ccf0ac27
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-07-28 21:08:00 +0900

    chore: 아이콘 에셋 생성 및 적용

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,8 @@
+/* 20X20 */
+export { default as IconHeartFilled20 } from '@/assets/icons/20/heart_filled.svg';
+export { default as IconPlus20 } from '@/assets/icons/20/plus.svg';
+
+/* 24X24 */
+export { default as IconHeartFilled24 } from '@/assets/icons/24/heart_filled.svg';
+export { default as IconHeartEmpty24 } from '@/assets/icons/24/heart_empty.svg';
+export { default as IconArrow24 } from '@/assets/icons/24/arrow.svg';
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { IconArrow24 } from '@/assets/icons';
 import { useRouter } from 'next/navigation';
 
 interface HeaderProps {
@@ -11,12 +12,12 @@ const Header = ({ title }: HeaderProps) => {
 
   return (
     <div className="relative flex h-[58px] w-full items-center justify-center">
-      <div
-        className="top-18 absolute left-20 h-24 w-24 cursor-pointer rounded-full bg-gray-100"
+      <IconArrow24
+        className="top-18 absolute left-20 h-24 w-24"
         onClick={() => {
           router.back();
         }}
-      ></div>
+      />
       <h1 className="title-20-bold">{title}</h1>
     </div>
   );
