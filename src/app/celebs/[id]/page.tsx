8027bb88eddf13110afabf5f498f9c6bcb8be5d8
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-08-01 23:42:08 +0900

    feat: 셀럽 페이지 소개글 뷰 구현

--- src/app/celebs/[id]/page.tsx
+++ src/app/celebs/[id]/page.tsx
@@ -1,14 +1,30 @@
 import Header from '@/components/Header';
 import CelebIntroductionSection from '../_client/CelebIntroductionSection';
+import { RestaurantData } from '@/@types';
+import Image from 'next/image';
 
-const CelebPageRestaurantCard = () => {
+const getCelebsRepresentationRestaurants = async (celebId: number): Promise<{ content: RestaurantData[] }> =>
+  (
+    await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/restaurants?lowLatitude=32&highLatitude=40&lowLongitude=120&highLongitude=132&sort=like&celebId=${celebId}`,
+    )
+  ).json();
+
+const CelebPageRestaurantCard = ({ name, images, category, roadAddress }: RestaurantData) => {
   return (
     <li className="flex gap-12">
-      <div className="h-[112px] w-[112px] rounded-[8px] bg-dim-5"></div>
+      <div className="relative h-[112px] w-[112px] overflow-hidden rounded-[8px] bg-gray-200">
+        <Image
+          src={`${process.env.NEXT_PUBLIC_S3_BASE_URL}/${images[0].name}.webp`}
+          fill
+          alt={name}
+          objectFit="cover"
+        />
+      </div>
       <div className="relative flex flex-1 flex-col">
-        <h3 className="mt-2 title-16-sb">식당 이름</h3>
-        <span className="mt-4 text-gray-400 caption-12-rg">음식 종류</span>
-        <span className="text-gray-800 body-13-rg">서울 성동구 연무장길</span>
+        <h3 className="mt-2 title-16-sb">{name}</h3>
+        <span className="mt-4 text-gray-400 caption-12-rg">{category}</span>
+        <span className="text-gray-800 body-13-rg">{roadAddress}</span>
 
         <p className="absolute bottom-0 left-0 text-gray-600 body-13-rg">식당 소개글 가나다라마바사아자차타파...</p>
       </div>
@@ -16,25 +32,24 @@ const CelebPageRestaurantCard = () => {
   );
 };
 
-const CelebPage = () => {
+const CelebPage = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  const data = await getCelebsRepresentationRestaurants(Number(id));
+
   return (
     <>
       <Header title="맛집 추천 리스트" />
       <main className="px-20 pt-16">
-        <CelebIntroductionSection />
+        <CelebIntroductionSection celebId={Number(id)} />
         <hr className="mt-28 h-1 w-full bg-gray-200" />
 
         <div className="mt-28 flex justify-between">
-          <h2 className="title-20-md">10개 매장</h2>
+          <h2 className="title-20-md">{data.content?.length}개 매장</h2>
           <span className="body-13-rg">최신순</span>
         </div>
 
         <ul className="mt-24 flex flex-col gap-20">
-          <CelebPageRestaurantCard />
-          <CelebPageRestaurantCard />
-          <CelebPageRestaurantCard />
-          <CelebPageRestaurantCard />
-          <CelebPageRestaurantCard />
+          {data.content?.map(props => <CelebPageRestaurantCard key={id} {...props} />)}
         </ul>
       </main>
     </>
--- src/app/celebs/_client/CelebIntroductionSection.tsx
+++ src/app/celebs/_client/CelebIntroductionSection.tsx
@@ -2,23 +2,31 @@
 
 import { IconHeartFilled20, IconPlus20 } from '@/assets/icons';
 import BottomSheet from '@/components/@ui/BottomSheet';
+import { useCelebProfiles } from '@/hooks/server';
 import Image from 'next/image';
+import Link from 'next/link';
 import { overlay } from 'overlay-kit';
 import { useState } from 'react';
 
-const PROFILE_IMAGE_URL =
-  'https://yt3.googleusercontent.com/vQrdlCaT4Tx1axJtSUa1oxp2zlnRxH-oMreTwWqB-2tdNFStIOrWWw-0jwPvVCUEjm_MywltBFY=s176-c-k-c0x00ffffff-no-rj';
+interface CelebIntroductionSectionProps {
+  celebId: number;
+}
 
-const CelebIntroductionSection = () => {
+const CelebIntroductionSection = ({ celebId }: CelebIntroductionSectionProps) => {
   const [isLiked, setIsLiked] = useState(false);
+
+  const { data } = useCelebProfiles(celebId);
 
   const openBottomSheet = () => {
     overlay.open(({ isOpen, close, unmount }) => {
       return (
         <BottomSheet open={isOpen} onClose={unmount} title="유튜브 채널 바로가기">
-          <button className="rounded-[8px]] flex h-56 w-full items-center justify-center gap-8 bg-gray-100">
-            <span className="body-16-md">성시경 채널 바로가기</span>
-          </button>
+          <Link
+            href={`https://www.youtube.com/${data?.youtubeChannelName}`}
+            className="rounded-[8px]] flex h-56 w-full items-center justify-center gap-8 bg-gray-100"
+          >
+            <span className="body-16-md">{data?.name} 채널 바로가기</span>
+          </Link>
           <button
             className="mt-16 flex h-56 w-full items-center justify-center gap-8 rounded-[8px] bg-gray-100"
             onClick={unmount}
@@ -34,19 +42,24 @@ const CelebIntroductionSection = () => {
     <>
       <div className="flex">
         <div className="flex-1">
-          <span className="title-22-md">셀럽 이름</span>
+          <span className="title-22-md">{data?.name}</span>
           <div className="mt-6 flex items-center">
             <span className="body-14-rg">구독자</span>
             <span className="ml-2 body-14-md">10만명</span>
             <span className="ml-12 body-14-rg">추천 매장</span>
             <span className="ml-2 body-14-md">50개</span>
           </div>
-          <p className="mt-14 pr-16 body-13-rg">
-            셀럽 소개글 가나다라마바사아자차카타파하 가나다라마바 최대 2줄 노출 후 말줄임 처리하기...셀럽 소개글
-            가나다라마바사아자차카타파하 가나다라마바 최대 2줄 노출 후 말줄임 처리하기...
-          </p>
+          <p className="mt-14 pr-16 body-13-rg">{data?.youtubeChannelName}</p>
         </div>
-        <Image className="h-72 w-72 rounded-full" src={PROFILE_IMAGE_URL} alt="성시경" width={72} height={72} />
+        {data && (
+          <Image
+            className="h-72 w-72 rounded-full"
+            src={data.profileImageUrl}
+            alt={data?.name}
+            width={72}
+            height={72}
+          />
+        )}
       </div>
       <div className="mt-20 flex gap-10">
         {isLiked ? (
