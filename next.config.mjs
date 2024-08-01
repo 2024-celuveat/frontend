fd3fd01243f46565aac4a41e43258c2845b6d4e7
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-08-01 23:34:18 +0900

    chore: CORS를 위한 proxy 설정

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+      },
+    ];
+  },
+
   webpack: config => {
     config.module.rules.push({
       test: /\.svg$/,
