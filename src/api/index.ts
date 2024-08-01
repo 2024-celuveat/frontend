a26a202aabe2bef8a155998322063215efc62f10
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-08-01 23:38:33 +0900

    feat: 셀럽 프로필 조회 로직 구현

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,14 @@
-import { RestaurantData } from '@/@types';
+import { Celeb, RestaurantData } from '@/@types';
 import axios from 'axios';
 
 export const getRestaurants = async ({ celebId }: { celebId: number }): Promise<{ content: RestaurantData[] }> => {
-  const data = await axios.get(
-    `/restaurants?lowLatitude=32&highLatitude=40&lowLongitude=120&highLongitude=132&sort=like&celebId=${celebId}&page=0`,
+  const response = await axios.get(
+    `/api/restaurants?lowLatitude=32&highLatitude=40&lowLongitude=120&highLongitude=132&sort=like&celebId=${celebId}&page=0`,
   );
-  return data.data;
+  return response.data;
+};
+
+export const getCelebProfiles = async (): Promise<Celeb[]> => {
+  const response = await axios.get(`/api/celebs`);
+  return response.data;
 };
--- src/hooks/server/index.ts
+++ src/hooks/server/index.ts
@@ -1,8 +1,15 @@
-import { getRestaurants } from '@/api';
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { getCelebProfiles, getRestaurants } from '@/api';
+import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
 
 export const useCelebsRepresentativeRestaurants = (celebId: number) =>
   useSuspenseQuery({
     queryKey: ['celebsRepresentativeRestaurants', celebId],
     queryFn: () => getRestaurants({ celebId }),
+  });
+
+export const useCelebProfiles = (celebId: number) =>
+  useQuery({
+    queryKey: ['celebProfiles'],
+    queryFn: getCelebProfiles,
+    select: data => data.find(({ id }) => id === celebId),
   });
