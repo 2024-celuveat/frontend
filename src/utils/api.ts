7fa95b288dcc602b4de1fe5cf60e9208ad4e4b92
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-08-24 23:54:03 +0900

    refactor: 비동기 추상화 함수 accessToken 설정 로직 추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,12 @@
 export const api = async (path: string, options?: RequestInit) => {
-  return await fetch(`${process.env.NEXT_PUBLIC_NEW_BASE_URL}${path}`, options);
+  const headers: RequestInit['headers'] = {};
+
+  if (typeof window !== 'undefined') {
+    headers['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`;
+  }
+
+  return await fetch(`${process.env.NEXT_PUBLIC_NEW_BASE_URL}${path}`, {
+    headers,
+    ...options,
+  });
 };
