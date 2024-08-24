54804218949a2a024150c02ce0c2a1c21c47c720
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-08-24 23:52:45 +0900

    feat: 레스토랑 API fetch 로직 생성

--- src/api/restaurant.api.ts
+++ src/api/restaurant.api.ts
@@ -1,16 +1,40 @@
 import { Restaurant } from '@/@types';
 import { api } from '@/utils/api';
 
+// 셀럽 추천 음식점 조회
 export const getRecommendedRestaurantsByCelebrities = async (): Promise<Restaurant[]> => {
   const response = await api(`/restaurants/celebrity/recommend`);
 
   return await response.json();
 };
 
+// 셀럽이 다녀간 음식점 조회
 export const getCelebrityRestaurants = async (
   celebrityId: number,
 ): Promise<{ contents: Restaurant[]; currentPage: number; hasNext: boolean; size: number }> => {
   const response = await api(`/restaurants/celebrity/${celebrityId}`);
 
   return await response.json();
+};
+
+// 관심 음식점 조회
+export const getInterestedRestaurant = async (): Promise<{
+  contents: Restaurant[];
+  currentPage: number;
+  hasNext: boolean;
+  size: number;
+}> => {
+  const response = await api('/restaurants/interested');
+
+  return await response.json();
+};
+
+// 관심 음식점 추가
+export const postInterestedRestaurant = async (restaurantId: number) => {
+  await api(`/restaurants/interested/${restaurantId}`, { method: 'POST' });
+};
+
+// 관심 음식점 삭제
+export const deleteInterestedRestaurant = async (restaurantId: number) => {
+  await api(`/restaurants/interested/${restaurantId}`, { method: 'DELETE' });
 };
