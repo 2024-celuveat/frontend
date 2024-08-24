f1cf23f925d8eb2c79fdaf5bf29301c5c7ec2a45
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-08-24 23:52:27 +0900

    feat: 셀럽 API fetch 로직 생성

--- src/api/celebrity.api.ts
+++ src/api/celebrity.api.ts
@@ -1,14 +1,33 @@
-import { BestCelebrities, CelebrityDetail } from '@/@types';
+import { BestCelebrities, Celebrity, CelebrityDetail } from '@/@types';
 import { api } from '@/utils/api';
 
+// 인기 셀럽 조회
 export const getBestCelebrities = async (): Promise<BestCelebrities[]> => {
   const response = await api(`/celebrities/best`);
 
   return await response.json();
 };
 
+// 셀럽 정보 조회
 export const getCelebrityInfo = async (celebrityId: number): Promise<CelebrityDetail> => {
   const response = await api(`/celebrities/${celebrityId}`);
 
   return await response.json();
+};
+
+// 관심 셀럽 조회
+export const getInterestedCelebrities = async (): Promise<CelebrityDetail['celebrity'][]> => {
+  const response = await api('/celebrities/interested');
+
+  return await response.json();
+};
+
+// 관심 셀럽 추가
+export const postInterestedCelebrity = async (celebrityId: number) => {
+  await api(`/celebrities/interested/${celebrityId}`, { method: 'POST' });
+};
+
+// 관심 셀럽 삭제
+export const deleteInterestedCelebrity = async (celebrityId: number) => {
+  await api(`/celebrities/interested/${celebrityId}`, { method: 'DELETE' });
 };
