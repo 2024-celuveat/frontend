7ebd3ab410ea775ff8e323e87c1ed3acf82a34bb
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-08-24 23:54:57 +0900

    feat: 셀럽 및 레스토랑 관련 비동기 함수 hook 생성

--- src/hooks/server/index.ts
+++ src/hooks/server/index.ts
@@ -1,11 +1,32 @@
+import { BestCelebrities } from '@/@types';
 import { SocialLoginType } from '@/@types/server/login.type';
-import { getAccessToken, getCelebrityInfo, getOAuthUrl } from '@/api';
+import {
+  deleteInterestedCelebrity,
+  deleteInterestedRestaurant,
+  getAccessToken,
+  getBestCelebrities,
+  getCelebrityInfo,
+  getInterestedCelebrities,
+  getInterestedRestaurant,
+  getOAuthUrl,
+  postInterestedCelebrity,
+  postInterestedRestaurant,
+} from '@/api';
 import { useMutation, useQuery, useSuspenseQuery } from '@tanstack/react-query';
+import { revalidateTag } from 'next/cache';
 
 export const useCelebrityInfoQuery = (celebrityId: number) =>
   useSuspenseQuery({
     queryKey: ['getCelebrityInfo'],
     queryFn: () => getCelebrityInfo(celebrityId),
+  });
+
+export const useBestCelebritiesQuery = ({ initialData }: { initialData: BestCelebrities[] }) =>
+  useSuspenseQuery({
+    queryKey: ['getBestCelebrities'],
+    queryFn: getBestCelebrities,
+    initialData,
+    refetchInterval: 10000,
   });
 
 export const useAccessTokenQuery = (socialLoginType: SocialLoginType, authCode: string) =>
@@ -21,5 +42,50 @@ export const useOauthUrlMutation = () =>
     mutationFn: getOAuthUrl,
     onSuccess: url => {
       window.location.href = url;
+      revalidateTag('restaurant');
     },
   });
+
+export const useInterestedRestaurantMutation = () => {
+  return useMutation({
+    mutationKey: ['getInterestedRestaurant'],
+    mutationFn: postInterestedRestaurant,
+  });
+};
+
+export const useDeleteInterestedRestaurantMutation = () => {
+  return useMutation({
+    mutationKey: ['getInterestedRestaurant'],
+    mutationFn: deleteInterestedRestaurant,
+  });
+};
+
+export const useInterestedRestaurantQuery = () =>
+  useQuery({
+    queryKey: ['getInterestedRestaurant'],
+    queryFn: getInterestedRestaurant,
+    staleTime: 0,
+    gcTime: 0,
+  });
+
+export const useInterestedCelebritiesQuery = () =>
+  useQuery({
+    queryKey: ['getInterestedCelebrities'],
+    queryFn: getInterestedCelebrities,
+    staleTime: 0,
+    gcTime: 0,
+  });
+
+export const useInterestedCelebrityMutation = () => {
+  return useMutation({
+    mutationKey: ['getInterestedCelebrity'],
+    mutationFn: postInterestedCelebrity,
+  });
+};
+
+export const useDeleteInterestedCelebrityMutation = () => {
+  return useMutation({
+    mutationKey: ['getInterestedCelebrity'],
+    mutationFn: deleteInterestedCelebrity,
+  });
+};
