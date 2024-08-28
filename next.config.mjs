40adb51b83a60e5aa1cd6160e6d51ddef1a271bc
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-08-28 23:31:11 +0900

    chore: 프록시 설정 제거

--- next.config.mjs
+++ next.config.mjs
@@ -9,14 +9,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
-      },
-    ];
-  },
   webpack: config => {
     config.module.rules.push({
       test: /\.svg$/,
