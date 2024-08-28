652c7ead3b7837d9724732f85202f5c86ab2b38b
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-08-29 08:24:54 +0900

    fix: 오탈자 수정

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -10,7 +10,7 @@ export const api = async (path: string, options?: RequestInit) => {
   }
 
   return await fetch(`${process.env.NEXT_PUBLIC_NEW_BASE_URL}${path}`, {
-    ...headers,
+    headers,
     ...options,
   });
 };
