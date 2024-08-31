debb2dc265c64b2455973e313a4c0f2d1e982f9d
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-08-31 11:04:50 +0900

    refactor: color 상수 모듈화

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import { transform } from 'next/dist/build/swc';
+import { colors } from './src/constants/colors';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
@@ -8,40 +8,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      white: {
-        DEFAULT: '#FFFFFF',
-      },
-      gray: {
-        50: '#F4F6F9',
-        100: '#F1F3F6',
-        200: '#E1E2E4',
-        300: '#BEBEC0',
-        400: '#909097',
-        600: '#6B6C74',
-        700: '#494A50',
-        800: '#2A2A2E',
-        900: '#101113',
-      },
-      dim: {
-        5: 'rgba(0, 0, 0, 0.05)',
-        50: 'rgba(0, 0, 0, 0.5)',
-        85: 'rgba(0, 0, 0, 0.86)',
-      },
-      main: {
-        500: '#FF7B54',
-        600: '#FF673A',
-        700: '#FC5524',
-      },
-      mainDim: {
-        15: '#FF7B5426',
-        8: '#FF7B5414',
-      },
-      sub: {
-        orange: '#FFB26C',
-        yellow: '#FFE48C',
-      },
-    },
+    ...colors,
     extend: {
       keyframes: {
         fadeIn: {
