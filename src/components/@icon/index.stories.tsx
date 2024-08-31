b98c3a93e6a34a3eb1afd52ed45d00e56756c9a3
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-08-31 11:41:49 +0900

    feat: 아이콘 스토리북 초안 작성

---
+++ src/components/@icon/index.stories.tsx
@@ -0,0 +1,57 @@
+import type { Meta } from '@storybook/react';
+import IconThumbsUp from './IconThumbsUp';
+import { colors } from '@/constants/colors';
+import IconArrowLeft from './IconArrowLeft';
+import IconArrowRight from './IconArrowRight';
+import IconCheck from './IconCheck';
+import IconClose from './IconClose';
+import IconHeartFilled from './IconHeartFilled';
+import IconHeartOutlined from './IconHeartOutlined';
+import IconHomeOutlined from './IconHomeOutlined';
+import IconHomeFilled from './IconHomeFilled';
+import IconInfo from './IconInfo';
+import IconLocation from './IconLocation';
+import IconMapOutlined from './IconMapOutlined';
+import IconMapFilled from './IconMapFilled';
+import IconPersonOutlined from './IconPersonOutlined';
+import IconPersonFilled from './IconPersonFilled';
+import IconNotice from './IconNotice';
+import IconPlus from './IconPlus';
+import IconMore from './IconMore';
+import IconReviewFilled from './IconReviewFilled';
+import IconReviewOutlined from './IconReviewOutlined';
+import IconSearch from './IconSearch';
+
+const meta: Meta = {
+  title: '@icon/index',
+};
+
+export default meta;
+
+export const Default = {
+  render: () => (
+    <>
+      <IconThumbsUp fill={colors.main[700]} />
+      <IconArrowLeft />
+      <IconArrowRight />
+      <IconCheck />
+      <IconClose />
+      <IconHeartFilled />
+      <IconHeartOutlined />
+      <IconHomeFilled />
+      <IconHomeOutlined />
+      <IconInfo />
+      <IconLocation />
+      <IconMapFilled />
+      <IconMapOutlined />
+      <IconPersonFilled />
+      <IconPersonOutlined />
+      <IconPlus />
+      <IconNotice />
+      <IconMore />
+      <IconReviewFilled />
+      <IconReviewOutlined />
+      <IconSearch />
+    </>
+  ),
+};
