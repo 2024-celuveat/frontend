6dbf9ad7bede7874fac5950e4da2e4ff9959ad53
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-09-07 11:25:52 +0900

    feat: 날짜 포맷팅 유틸 함수

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,9 @@
+export const formatDate = (isoDate: string) => {
+  const date = new Date(isoDate);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate() - 1).padStart(2, '0');
+
+  return `${year}.${month}.${day}`;
+};
