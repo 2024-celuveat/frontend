1d6a38d669534d4ea7c9ceebe9c2068d5b188741
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-09-18 12:08:05 +0900

    refactor: 유저 프로필 응답 타입 고도화

--- src/@types/server/user.type.ts
+++ src/@types/server/user.type.ts
@@ -1,9 +1,11 @@
+import { SocialLoginType } from './login.type';
+
 export interface UserProfile {
   id: number;
   nickname: string;
   profileImageUrl: string;
   email: string;
-  serverType: string;
+  serverType: SocialLoginType;
   socialId: string;
   interestedCount: number;
   reviewCount: number;
