71fc48c7703c0b3f1c0734a1c53954433147fffc
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-09-22 13:05:14 +0900

    refactor: 셀럽 관심 추가및 삭제시 revalidate

--- src/app/(actions)/celebs/actions.ts
+++ src/app/(actions)/celebs/actions.ts
@@ -1,22 +1,31 @@
+'use server';
+
+import { revalidateTag } from 'next/cache';
+
 import { CelebrityDetail } from '@/@types';
+import { TAGS } from '@/constants/tags';
 import { api } from '@/utils/api';
 
 // 셀럽 정보 조회
 export const getCelebrityInfo = async (celebrityId: number): Promise<CelebrityDetail> => {
-  return api(`/celebrities/${celebrityId}`);
+  return api(`/celebrities/${celebrityId}`, { next: { tags: [TAGS.TYPE.CELEBRITY] } });
 };
 
 // 관심 셀럽 추가
 export const postInterestedCelebrity = async (celebrityId: number) => {
   await api(`/celebrities/interested/${celebrityId}`, { method: 'POST' });
+
+  revalidateTag(TAGS.TYPE.CELEBRITY);
 };
 
 // 관심 셀럽 삭제
 export const deleteInterestedCelebrity = async (celebrityId: number) => {
   await api(`/celebrities/interested/${celebrityId}`, { method: 'DELETE' });
+
+  revalidateTag(TAGS.TYPE.CELEBRITY);
 };
 
 // 관심 셀럽 조회
 export const getInterestedCelebrities = async (): Promise<CelebrityDetail['celebrity'][]> => {
-  return await api('/celebrities/interested');
+  return await api('/celebrities/interested', { next: { tags: [TAGS.TYPE.CELEBRITY] } });
 };
