439b7c72881470d210a1bd61d1297affaaa7ec02
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-09-23 00:45:13 +0900

    refactor: 토스트 ui 수정

--- src/components/@ui/Toast/Toast.tsx
+++ src/components/@ui/Toast/Toast.tsx
@@ -18,7 +18,7 @@ function Toast({ open, onClose, children }: ToastProps) {
   }, [open, onClose]);
 
   return open ? (
-    <div className="fixed bottom-[100px] left-1/2 -translate-x-1/2 animate-toast rounded-[8px] bg-dim-85 px-32 py-16 text-white body-14-rg">
+    <div className="fixed bottom-[100px] left-1/2 -translate-x-1/2 animate-toast text-nowrap rounded-[8px] bg-dim-85 px-32 py-16 text-white body-14-rg">
       {children}
     </div>
   ) : null;
