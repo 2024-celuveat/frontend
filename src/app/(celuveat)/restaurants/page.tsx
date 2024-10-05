48182e62564d194357473faec23134a47a8c33f2
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-10-05 11:12:58 +0900

    feat: 카테고리별 음식점 갯수 조회 기능

--- src/app/(celuveat)/restaurants/page.tsx
+++ src/app/(celuveat)/restaurants/page.tsx
@@ -1,13 +1,18 @@
-import { getRestaurants } from '@/app/(actions)/restaurants/actions';
+import { getRestaurants, getRestaurantsCount } from '@/app/(actions)/restaurants/actions';
 import RestaurantCardRow from '@/components/RestaurantCardRow';
 
 async function RestaurantListPage({ searchParams }: { searchParams: { category: string } }) {
-  const restaurants = await getRestaurants({ category: searchParams.category, page: 0, size: 10, sort: [] });
+  const restaurantsData = await getRestaurants({ category: searchParams.category, page: 0, size: 10, sort: [] });
+  const restaurantsCountData = await getRestaurantsCount({
+    category: searchParams.category,
+  });
+
+  const [restaurants, restaurantsCount] = await Promise.all([restaurantsData, restaurantsCountData]);
 
   return (
     <main className="px-20">
       <div className="mt-28 flex justify-between">
-        <h2 className="title-20-md">{restaurants.contents?.length}개 매장</h2>
+        <h2 className="title-20-md">{restaurantsCount}개 매장</h2>
         <span className="body-13-rg">최신순</span>
       </div>
       <ul className="mt-24 flex flex-col gap-20">
