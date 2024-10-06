828af37eaf8e24cc79203c5d9ad9925b6a6ac022
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-10-06 12:24:55 +0900

    feat: useInfiniteScroll hook

---
+++ src/hooks/useInfiniteScroll.ts
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from 'react';
+
+interface UseInfiniteScrollProps {
+  eventHandler: () => void;
+  observerOptions?: IntersectionObserverInit;
+}
+
+const useInfiniteScroll = ({ eventHandler, observerOptions }: UseInfiniteScrollProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        eventHandler();
+      }
+    }, observerOptions);
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+  }, [ref]);
+
+  return ref;
+};
+
+export default useInfiniteScroll;
