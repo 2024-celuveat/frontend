2dc029315dbb564f0d43ef6c35e75f898394c86a
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-10-17 23:58:04 +0900

    refactor: middleware 최적화

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,6 @@
 import { NextResponse, type NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  if (request.cookies.has('accessToken')) {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_SERVER_BASE_URL}/members/profile`, {
-      headers: {
-        Authorization: `Bearer ${request.cookies.get('accessToken')?.value}`,
-      },
-    });
-
-    if (response.status === 401) {
-      const nextResponse = NextResponse.next();
-      nextResponse.cookies.delete('accessToken');
-      return nextResponse;
-    }
-  }
-
   const headers = new Headers(request.headers);
   headers.set('Set-Cookie', request.cookies.get('accessToken')?.value ?? '');
 
