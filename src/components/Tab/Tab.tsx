24ba3ae18f221f8a23d89aa74de2917c97bbff3d
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-11-01 00:23:34 +0900

    refactor: 셀럽필터링 탭 이미지 UI 개선

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -15,7 +15,9 @@ function Tab({ label, imageUrl, isActive = false, ...props }: TabProps) {
       {...props}
       className={`flex h-40 w-fit flex-none items-center justify-center gap-[5px] rounded-[100px] border ${imageUrl ? 'pl-6 pr-12' : 'px-14'} ${isActive && 'border-[1.5px] border-main-500'}`}
     >
-      {imageUrl && <Image className="rounded-full" src={imageUrl} alt={label} width={24} height={24} />}
+      {imageUrl && (
+        <Image className="aspect-square rounded-full object-cover" src={imageUrl} alt={label} width={24} height={24} />
+      )}
       <span className={`text-gray-800 body-15-rg ${isActive && 'font-[700_!important] text-main-500'}`}> {label}</span>
     </div>
   );
