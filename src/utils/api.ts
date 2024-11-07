84238edd235d06c5c79718bf7f0f4f95b694ee17
Jeremy <102432453+shackstack@users.noreply.github.com>
2024-11-07 23:53:27 +0900

    Revert "refactor: api 유틸함수 cookies 로직 제거"
    This reverts commit ba8d2c7ddcc02fbcad118c2744ec8a7cc378383f.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,12 @@
 'use server';
 
+import { cookies } from 'next/headers';
+
 export const api = async <T>(url: string, options?: Omit<RequestInit, 'body'> & { data?: unknown }): Promise<T> => {
   const headers: RequestInit['headers'] = {};
-
+  if (cookies().has('accessToken')) {
+    headers.Authorization = `Bearer ${cookies().get('accessToken')?.value}`;
+  }
   const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_SERVER_BASE_URL}${url}`, {
     ...options,
     headers: {
@@ -12,6 +16,8 @@ export const api = async <T>(url: string, options?: Omit<RequestInit, 'body'> & 
     },
     body: JSON.stringify(options?.data),
   });
+
+  if (!response.ok) throw response;
 
   const contentType = response.headers.get('content-type');
   if (contentType === 'application/json') return response.json();
